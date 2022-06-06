9f44b1216972f50b898477837001007dfce4c2aa
Jeewoo <cheltus11@gmail.com>
2022-06-06 19:30:23 +0900

    [main] 기본 기능 동작

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,9 +16,9 @@ public class JpaMain {
 		tx.begin();
 		
 		try {
-//			Member member = new Member();
-//			member.setId(1L);
-//			member.setName("HelloA");
+			Member member = new Member();
+			member.setId(1L);
+			member.setName("HelloA");
 			
 			Member findmember = em.find(Member.class, 1L);
 			System.out.println("findMember member name " + findmember.getName());
